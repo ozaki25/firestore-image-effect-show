fb97bb97c694785a88b5e5858f85c6864932434b
ozaki <fs65ugumori@yahoo.co.jp>
2019-09-19 08:30:42 +0900

    subscribe firestore

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from 'react';
 import images from './constants/images';
 import effects from './constants/effects';
 import Image from './components/Image';
+import Firestore from './utils/firestore';
 
 function getRandom(list) {
   return list[Date.now() % list.length];
@@ -11,21 +12,20 @@ function App() {
   const [oldSlide, setOldSlide] = useState(null);
   const [currentSlide, setCurrentSlide] = useState(null);
 
-  const onClick = () => {
+  const onReceive = ({ url, caption, comment }) => {
+    console.log({ url, caption, comment });
     setOldSlide(currentSlide);
-    setCurrentSlide({
-      url: getRandom(images),
-      effect: getRandom(effects),
-    });
+    setCurrentSlide({ url, effect: getRandom(effects) });
   };
 
   useEffect(() => {
-    onClick();
+    const firestore = new Firestore();
+    firestore.subscribe({ ref: firestore.dbImagesRef, onReceive });
   }, []);
 
   console.log({ oldSlide, currentSlide });
   return (
-    <div onClick={onClick}>
+    <div>
       {oldSlide && <Image key={performance.now()} {...oldSlide} type="out" />}
       {currentSlide && (
         <Image key={performance.now()} {...currentSlide} type="in" />
